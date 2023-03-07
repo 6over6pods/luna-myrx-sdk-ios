5e20078fbdd0863f2b3d3372b2cd4a0f7c724a72
Eugene Gordenstein <eugene@6over6.com>
2023-03-07 16:12:46 +0200

    Add Sample App

---
+++ SampleApp/MyRxSampleApp/MyRxSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRxSampleAppApp.swift
+//  MyRxSampleApp
+//
+//  Created by Eugene Gordenstein on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyRxSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
